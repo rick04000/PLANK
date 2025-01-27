1fa6e8f9376d0e50dc5998cdc48a8dfc96eecbfc
rick04000 <133609023+rick04000@users.noreply.github.com>
2025-01-27 02:46:45 +0100

    asdf

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-import * as THREE from '../node_modules/three/build/three.module.js';
-import { GLTFLoader } from '../node_modules/three/examples/jsm/loaders/GLTFLoader.js';
-import { OrbitControls } from '../node_modules/three/examples/jsm/controls/OrbitControls.js';
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 
 /******************************************************
